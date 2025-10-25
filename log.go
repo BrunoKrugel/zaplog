055307525f2f5f89fddf08c1a671137517305bda
Bruno Krugel <brunokrugel@gmail.com>
2025-10-24 22:41:45 -0300

    Add missing methods

--- log.go
+++ log.go
@@ -2,6 +2,8 @@ package zaplog
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -14,13 +16,29 @@ type Logger struct {
 var globalLogger *Logger
 
 func StartLogger() error {
-	config := zap.NewProductionConfig()
-	config.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
-
-	logger, err := config.Build(zap.AddCaller(), zap.AddCallerSkip(1))
-	if err != nil {
-		return err
+	cfg := zapcore.EncoderConfig{
+		TimeKey:          "dt",
+		LevelKey:         "level",
+		MessageKey:       "message",
+		CallerKey:        "caller",
+		StacktraceKey:    "stack_trace",
+		ConsoleSeparator: " ",
+		EncodeLevel:      zapcore.LowercaseLevelEncoder,
+		EncodeTime:       zapcore.RFC3339NanoTimeEncoder,
+		EncodeCaller:     zapcore.ShortCallerEncoder,
 	}
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(cfg),
+		zapcore.AddSync(os.Stdout),
+		zap.InfoLevel,
+	)
+
+	logger := zap.New(core,
+		zap.AddCaller(),
+		zap.AddCallerSkip(1),
+		zap.AddStacktrace(zap.ErrorLevel),
+	)
 
 	globalLogger = &Logger{Logger: logger}
 	return nil
@@ -50,14 +68,30 @@ func Info(ctx context.Context, message string, fields ...zap.Field) {
 	GetGlobalLogger().WithContext(ctx).Info(message, fields...)
 }
 
+func Infof(ctx context.Context, format string, args ...any) {
+	GetGlobalLogger().WithContext(ctx).Info(fmt.Sprintf(format, args...))
+}
+
 func Debug(ctx context.Context, message string, fields ...zap.Field) {
 	GetGlobalLogger().WithContext(ctx).Debug(message, fields...)
+}
+
+func Debugf(ctx context.Context, format string, args ...any) {
+	GetGlobalLogger().WithContext(ctx).Debug(fmt.Sprintf(format, args...))
 }
 
 func Error(ctx context.Context, message string, fields ...zap.Field) {
 	GetGlobalLogger().WithContext(ctx).Error(message, fields...)
 }
 
+func Errorf(ctx context.Context, format string, args ...any) {
+	GetGlobalLogger().WithContext(ctx).Error(fmt.Sprintf(format, args...))
+}
+
 func Fatal(ctx context.Context, message string, fields ...zap.Field) {
 	GetGlobalLogger().WithContext(ctx).Fatal(message, fields...)
+}
+
+func Fatalf(ctx context.Context, format string, args ...any) {
+	GetGlobalLogger().WithContext(ctx).Fatal(fmt.Sprintf(format, args...))
 }
